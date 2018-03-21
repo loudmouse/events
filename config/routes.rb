3693b61d7af35f0f914f8aa44822db6a4fc77ef6
Nolan Neeley <nolanneeley@gmail.com>
2018-03-21 06:06:54 -0500

    add user instance variable to show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
   resources :users
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
