a01e7d62ac75300feaf674c27d6c325702056d20
Nolan Neeley <nolanneeley@gmail.com>
2018-03-21 22:04:07 -0500

    add Event model with attributes for date, location, and description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321022211) do
+ActiveRecord::Schema.define(version: 20180322025815) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "location"
+    t.date "date"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
