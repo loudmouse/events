e6ce13ea717943e8149e9396e5da5496d1847bae
Nolan Neeley <nolanneeley@gmail.com>
2018-03-21 22:37:17 -0500

    generated Event controller with new, create, show, index actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'events/new'
+
+  get 'events/create'
+
+  get 'events/show'
+
+  get 'events/index'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   resources :users
